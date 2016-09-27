1b22ff9dcb07469193df45ad50fec0bf1412aa23
Jim Shapleigh <jps2@cornell.edu>
2016-09-26 20:54:52 -0400

    TN > JS

--- counts.sh
+++ counts.sh
@@ -1,4 +1,4 @@
 echo "count,filename" > seq_counts.csv
-for f in ./TN*.fastq.gz; do
+for f in ./JC*.fastq.gz; do
     cat $f | bioawk -c fastx '{print $name}' | wc -l | awk -v f=$f 'BEGIN{OFS=","}{print $0, f}';
 done >> seq_counts.csv
